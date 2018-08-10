9e0a25fe73e133caa6b4471039b612f8c671ab1b
zhanglili <373602426@qq.com>
2018-08-10 16:48:52 +0800

    no message

--- demo_join/demo_join/CreateRoomViewController.m
+++ demo_join/demo_join/CreateRoomViewController.m
@@ -89,7 +89,7 @@
     option.roomDisconnectListener = liveRoomVC;
     
     // 该参数代表进房之后使用什么规格音视频参数，参数具体值为客户在腾讯云实时音视频控制台画面设定中配置的角色名（例如：默认角色名为user, 可设置controlRole = @"user"）
-    option.controlRole = @"zll1";
+    option.controlRole = @"zll2";
     
     // 3. 调用创建房间接口，传入房间ID和房间配置对象
     [[ILiveRoomManager getInstance] joinRoom:[self.roomIDTF.text intValue] option:option succ:^{
