f651d6ff402d3cfb24381f901b1dde4e8605ed52
zhanglili <373602426@qq.com>
2018-08-10 15:21:25 +0800

    no message

---
+++ demo_join/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  demo_join
+//
+//  Created by zhanglili on 2018/8/7.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constant : NSObject
+
+@end
---
+++ demo_join/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  demo_join
+//
+//  Created by zhanglili on 2018/8/7.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
