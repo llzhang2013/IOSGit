92a9df71e1563dcb2e633b10cd6d25a6fae30edc
zhanglili <373602426@qq.com>
2018-09-25 11:43:30 +0800

    no message

--- JSStudy/one/study/index.js
+++ JSStudy/one/study/index.js
@@ -1,5 +1,5 @@
 
-console.log('**************************', );
+console.log('**************************');
 
 var pattern = new RegExp('Box','i');
 var str = 'box';
@@ -12,7 +12,7 @@ var str = 'This is a box';
 console.log(pattern.test(str)); //true
 var pattern = /Box/i;
 var str = 'gggbox';
-console.log(pattern.exec(str)); //[ 'box', index: 0, input: 'box' ]
+console.log(pattern.exec(str)); //[ 'box', index: 3, input: 'gggbox' ]
 //box返回的是数组，有就返回数组的值，没有匹配到就返回null
 var str ='ss';
 console.log(pattern.exec(str)); //没有匹配到返回null
--- JSStudy/one/study/reqesp.js
+++ JSStudy/one/study/reqesp.js
@@ -1,15 +1,1 @@
-var pattern = new RegExp('Box','i');
-var str = 'box';
-console.log(pattern.test(str)); //true
-var pattern = /Box/i;
-var str = 'box';
-console.log(pattern.test(str)); //true
-var pattern = /Box/i;
-var str = 'This is a box';
-console.log(pattern.test(str)); //true
-var pattern = /Box/i;
-var str = 'box';
-console.log(pattern.exec(str)); box
-//返回的是数组，有就返回数组的值，没有匹配到就返回null
-var str ='ss';
-//console.log(pattern.exec(str)); 没有匹配到返回null
+
